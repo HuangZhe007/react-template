51c761f88b66f6dedd90c4e76ba593369b659d3b
HuangZhe007 <315771855@qq.com>
2021-12-10 11:47:13 +0800

    perf: ⚡️ craco config

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,30 @@
 /* eslint-disable */
 const CracoAntDesignPlugin = require('craco-antd');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const { NODE_ENV } = process.env;
+
+const Webpack = {
+  production: {
+    plugins: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+            pure_funcs: ['console.log'],
+          },
+        },
+      }),
+      // Ignore all local files of moment.js
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    ],
+  },
+  development: {},
+};
 
 module.exports = {
+  devServer: {},
   plugins: [
     {
       plugin: CracoAntDesignPlugin,
@@ -10,4 +33,5 @@ module.exports = {
       },
     },
   ],
+  webpack: Webpack[NODE_ENV],
 };
