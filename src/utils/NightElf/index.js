7be121f6b605c63311aea6c9f6f60f5c1c9713f9
HuangZhe007 <315771855@qq.com>
2021-12-28 15:31:24 +0800

    ci: 🎡 VConsole

--- src/utils/NightElf/index.js
+++ src/utils/NightElf/index.js
@@ -1,4 +1,6 @@
 import NightElfCheck from './NightElfCheck';
 import AelfBridgeCheck from '../AelfBridge/AelfBridgeCheck';
 import { isMobile } from 'react-device-detect';
+import VConsole from 'vconsole';
+process.env.NODE_ENV === 'development' && isMobile && new VConsole();
 export const NightElf = isMobile ? AelfBridgeCheck : NightElfCheck;
