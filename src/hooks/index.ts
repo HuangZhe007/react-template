05ac39abc37fdc844950c50c02e37b233c164774
HuangZhe007 <315771855@qq.com>
2021-12-30 17:01:56 +0800

    docs: ✏️ contexts & hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,30 @@
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
+
+/**
+ * useUnmounted
+ * @returns boolean
+ * whether the component is unmounted
+ */
+export function useUnmounted() {
+  const unmountedRef = useRef(false);
+  useEffect(() => {
+    return () => {
+      unmountedRef.current = true;
+    };
+  }, []);
+  return unmountedRef.current;
+}
+/**
+ * @method useAsyncState
+ * Prevent React state update on an unmounted component.
+ */
+export function useAsyncState<S>(initialState?: S | (() => S)): [S | undefined, Dispatch<SetStateAction<S>>] {
+  const unmountedRef = useUnmounted();
+  const [state, setState] = useState(initialState);
+  const setAsyncState = useCallback((s) => {
+    if (unmountedRef) return;
+    setState(s);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return [state, setAsyncState];
+}
